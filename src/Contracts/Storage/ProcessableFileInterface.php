7feb4787bd50c597252c1d2f30f3ca11d48ba5f4
Coen Zimmerman <tabun@xs4all.nl>
2017-08-09 23:33:58 +0200

    Updated processable file interface

--- src/Contracts/Storage/ProcessableFileInterface.php
+++ src/Contracts/Storage/ProcessableFileInterface.php
@@ -5,6 +5,12 @@ interface ProcessableFileInterface extends StorableFileInterface
 {
 
     /**
+     * @param \SplFileInfo|string $data     file or path
+     * @return $this
+     */
+    public function setData($data);
+
+    /**
      * Sets a new mime type for the processed file.
      *
      * @param string $mimeType
